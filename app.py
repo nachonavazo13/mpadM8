648011f8a1a0967a4d1a3a2edb560ad1a36e7908
nachonavazo13 <nachonavazo13@hotmail.com>
2024-11-26 16:50:46 -0300

    modificacion de prueba

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.title('Hola desde Streamlit Cloud')
 
 # Boton para mostrar el mensaje.
 if st.button('Mostrar mensaje'):
-    st.write('Vos sos amigo de Karina y de Pupo!')
+    st.write('Vos sos amigo de Karina y de Pupoooooooooooooooooo!')
